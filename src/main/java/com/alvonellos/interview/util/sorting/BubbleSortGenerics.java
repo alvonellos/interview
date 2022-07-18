d0c5cb084f09b90feb01d5dc4c21171c6abeeca8
Alexander Alvonellos <alexander.alvonellos@gmail.com>
2022-07-18 12:46:44 -0400

    Fixed bubble sort generics and added a test

--- src/main/java/com/alvonellos/interview/util/sorting/BubbleSortGenerics.java
+++ src/main/java/com/alvonellos/interview/util/sorting/BubbleSortGenerics.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 
-public class BubbleSortGenerics {
+public class BubbleSortGenerics<E> {
 
     <E> void swap(E[] a, int i, int j) {
         if (i != j) {
@@ -14,11 +14,11 @@ public class BubbleSortGenerics {
         }
     }
 
-    public <E extends Comparable<E>> void BubbleSort(E[] a) {
+    public  <E extends Comparable<E>> void BubbleSort(E[] a) {
         for (int i = 0; i < a.length - 1; i++) {
-        	for(int j = 0; i<a.length - i - 1; j++) { 
-                if (a[j].compareTo(a[j])<=0) {
-                	swap(a, i, j);
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j].compareTo(a[j + 1]) > 0) {
+                    swap(a, j, j + 1);
                 }
             }
         }
--- src/test/java/com/alvonellos/interview/util/sorting/BubbleSortGenericsTest.java
+++ src/test/java/com/alvonellos/interview/util/sorting/BubbleSortGenericsTest.java
@@ -1,12 +1,57 @@
 package com.alvonellos.interview.util.sorting;
 
+import lombok.var;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.shadow.com.univocity.parsers.common.NormalizedString.toArray;
 
 class BubbleSortGenericsTest {
+    SecureRandom random = new SecureRandom();
+    static final int STREAM_SIZE = 1024;
+    @RepeatedTest(10)
+    @Timeout(100)
+    void bubbleSort() {
+
+        Integer[] a = random.ints(STREAM_SIZE).boxed().toArray(Integer[]::new);
+        List<Integer> b = new ArrayList<>(Arrays.asList(a));
+
+        BubbleSortGenerics bubbleSort = new BubbleSortGenerics<Integer>();
+        bubbleSort.BubbleSort(a);
+
+        System.err.println(Arrays.stream(a).collect(Collectors.toList()));
+        System.err.println(b.stream().sorted().collect(Collectors.toList()));
+        assertSorted(a);
+    }
+
+    void assertSorted(Integer[] a) {
+        assertArrayEquals(a, Arrays.stream(a).sorted().toArray());
+    }
 
     @Test
-    void bubbleSort() {
+    void swap() {
+        Integer[] a = random.ints(STREAM_SIZE).boxed().toArray(Integer[]::new);
+        List<Integer> b = new ArrayList<>(Arrays.asList(a));
+
+        BubbleSortGenerics bubbleSort = new BubbleSortGenerics<Integer>();
+        bubbleSort.BubbleSort(a);
+
+        bubbleSort.swap(a, 0, 1);
+
+        System.err.println(Arrays.stream(a).collect(Collectors.toList()));
+        System.err.println(b.stream().sorted().collect(Collectors.toList()));
+
+        assert(a[0]>a[1]);
+
     }
 }
