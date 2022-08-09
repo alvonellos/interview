f4d6e021603dfd22cb245050535307bda670872e
Alexander Alvonellos <alexander.alvonellos@gmail.com>
2022-08-09 14:45:05 -0400

    Implement roman numeral and roman numerals test. Added a leetcode solution too in this branch

--- src/test/java/com/alvonellos/interview/util/numbers/NumberManipulationTest.java
+++ src/test/java/com/alvonellos/interview/util/numbers/NumberManipulationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static com.alvonellos.interview.util.numbers.NumberManipulation.randBetween;
+import static com.alvonellos.interview.util.numbers.NumberManipulation.twoSum;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -12,5 +13,14 @@ class NumberManipulationTest {
     @Test
     void randBetweenTest() {
         assertTrue(randBetween(1L, 10L) >= 1L && randBetween(1L, 10L) <= 10L);
+    }
+
+    @Test
+    void twoSumTest() {
+        int[] expected = new int[] {0,1};
+        int[] actual = new int[] {2,7,11,15};
+
+        int[] output = twoSum(actual, 9);
+        assertArrayEquals(expected, output);
     }
 }
