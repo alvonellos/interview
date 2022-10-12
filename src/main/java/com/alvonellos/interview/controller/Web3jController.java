428c92cf11ff8df82fac9dfb5fc5162fc80bcb5c
Alexander Alvonellos <alexander.alvonellos@gmail.com>
2022-10-12 13:08:20 -0400

    added web3j examples

--- src/main/java/com/alvonellos/interview/controller/Web3jController.java
+++ src/main/java/com/alvonellos/interview/controller/Web3jController.java
@@ -6,9 +6,13 @@ import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.web3j.crypto.CipherException;
 
+import javax.validation.constraints.NotBlank;
 import java.io.IOException;
+import java.math.BigInteger;
 
 @RestController  // Because of SpringBoot ResponseBody implementation. We are not returning a view.
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -23,13 +27,24 @@ public class Web3jController {
     }
 
     @GetMapping("/gasPrice")
-    public ResponseEntity<String> getGasPrice() throws IOException {
+    public ResponseEntity<BigInteger> getGasPrice() throws IOException {
         return ResponseEntity.ok(Web3jService.getGasPrice());
     }
 
-    @GetMapping("/balance")
-    public ResponseEntity<String> getBalance() throws IOException {
-        return ResponseEntity.ok(Web3jService.getBalance());
+    @GetMapping("/balance/{address}")
+    public ResponseEntity<String> getBalance(@NotBlank @PathVariable(name = "address") String address) throws IOException, CipherException {
+        return ResponseEntity.ok(Web3jService.getBalance(address));
     }
 
+    @GetMapping("/admin")
+    public ResponseEntity<String> getAdmin() throws IOException {
+        return ResponseEntity.ok(Web3jService.getAdmin());
+    }
+
+    @GetMapping("/transfer/{from}/{to}/{amount}")
+    public ResponseEntity<String> transfer(@NotBlank @PathVariable(name = "from") String from,
+                                           @NotBlank @PathVariable(name = "to") String to,
+                                           @NotBlank @PathVariable(name = "amount") BigInteger amount) throws IOException, CipherException {
+        return ResponseEntity.ok(Web3jService.ethTransfer(from, to, amount));
+    }
 }
