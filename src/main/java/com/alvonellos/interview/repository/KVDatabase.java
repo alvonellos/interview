5a66545f146534a5aeda2a0a130c8a0b15a5e41b
Alexander Alvonellos <alexander.alvonellos@gmail.com>
2022-10-22 10:21:39 -0400

    implemented twilio

--- src/main/java/com/alvonellos/interview/repository/KVDatabase.java
+++ src/main/java/com/alvonellos/interview/repository/KVDatabase.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface KVDatabase extends JpaRepository<KVEntity, Long> {
+    KVEntity findByKVEntityKey(String key);
+
 }
