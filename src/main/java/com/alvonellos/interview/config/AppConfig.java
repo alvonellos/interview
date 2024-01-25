0be57e9d724ffe8b0d842bdd6b5f9afb0fa9571c
Alexander Alvonellos <alexander.alvonellos@gmail.com>
2024-01-24 23:26:31 -0500

    This builds without the added dependency, and made some minor changes for logging to work.

--- src/main/java/com/alvonellos/interview/config/AppConfig.java
+++ src/main/java/com/alvonellos/interview/config/AppConfig.java
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Clock;
@@ -17,7 +18,7 @@ public class AppConfig {
   public RestTemplate restTemplate() {
     RestTemplate restTemplate = new RestTemplate();
 
-    restTemplate.setRequestFactory(new OkHttp3ClientHttpRequestFactory(new OkHttpClient()));
+    restTemplate.setRequestFactory(new SimpleClientHttpRequestFactory());
 
     return restTemplate;
   }
